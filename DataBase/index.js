4fff8bf58bb34e3cac90ef246a7fbb136f53fbd6
itachi880 <bdortiana880@gmail.com>
2024-10-27 11:03:51 +0100

    fix connection bug

--- DataBase/index.js
+++ DataBase/index.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose");
-const db = mongoose.createConnection(process.env.MONGODB_URL, { dbName: process.env.DB_NAME });
+const db = mongoose.connect(process.env.MONGODB_URL, { dbName: process.env.DB_NAME });
 module.exports = db;
